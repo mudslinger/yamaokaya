e59104bd706d6a74d4a2465f3cb9accc21563feb
hisato tanaka <hisato.tanaka@gmail.com>
2014-06-27 11:20:17 +0900

    domain change

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 DOMAINS = {
-	corporate: 'yam.jp',
-	yamaokaya: 'yam.com',
-	recruit:   'recruit.yam.com'
+	corporate: 'maruchiyo2014.yamaokaya.com',
+	yamaokaya: 'www2014.yamaokaya.com',
+	recruit:   'recruit2014.yamaokaya.com'
 }
