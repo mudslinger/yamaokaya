267d3253d9bea10d6b7069b35de7eef530d1e411
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-04 20:25:46 +0900

    fix

--- config/initializers/spo_auth.rb
+++ config/initializers/spo_auth.rb
@@ -1,1 +1,1 @@
-SPO_AUTHXML =  RestClient.get 'http://neptune:7700/ssconnect/trigger/spo/credentials.xml'
+SPO_AUTHXML =  RestClient.get 'http://192.168.11.30:7700/ssconnect/trigger/spo/credentials.xml'
