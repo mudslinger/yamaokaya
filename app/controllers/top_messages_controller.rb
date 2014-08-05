3be582c627ac44e33c3c60be4312ecd5df185797
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-05 18:03:04 +0900

    wbmes

--- app/controllers/top_messages_controller.rb
+++ app/controllers/top_messages_controller.rb
@@ -4,13 +4,17 @@ class TopMessagesController < ApplicationController
   layout false
 
   def show
-      @type = params[:type].to_sym if  params[:type].present?
-      @key = 0
-      @key = params[:key].to_i if params[:key].present?
 
-      list = TopMessage.articles(@type)
-      @entry = list[@key]
-      @prev_entry = @key == 0 ? nil : list[@key-1]
-      @next_entry = @key < list.size ? list[@key+1] : nil
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET"
+    @type = params[:type].to_sym if  params[:type].present?
+    @key = 0
+    @key = params[:key].to_i if params[:key].present?
+
+    list = TopMessage.articles(@type)
+    @entry = list[@key]
+    @prev_entry = @key == 0 ? nil : list[@key-1]
+    @next_entry = @key < list.size ? list[@key+1] : nil
   end
 end
