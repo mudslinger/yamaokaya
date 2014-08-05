1643d86024d388b3868765551a27aa6582a7dbfb
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-05 15:52:58 +0900

    wbmessage

--- app/controllers/top_messages_controller.rb
+++ app/controllers/top_messages_controller.rb
@@ -9,7 +9,7 @@ class TopMessagesController < ApplicationController
       @key = params[:key].to_i if params[:key].present?
 
       list = TopMessage.articles(@type)
-      @entry = list[key]
+      @entry = list[@key]
       @prev_entry = @key == 0 ? nil : list[@key-1]
       @next_entry = @key < list.size ? list[@key+1] : nil
   end
