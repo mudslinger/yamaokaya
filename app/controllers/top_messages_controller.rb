4ce08937b2bdaa96039c5523a106a315ecb90f5c
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-05 12:39:06 +0900

    topmessage

--- app/controllers/top_messages_controller.rb
+++ app/controllers/top_messages_controller.rb
@@ -3,57 +3,14 @@ class TopMessagesController < ApplicationController
   skip_before_filter :set_title
   layout false
 
-  def authentication
-    ActiveSupport::XmlMini.backend = 'Nokogiri'
-    xml = RestClient.post(
-      "https://login.microsoftonline.com/extSTS.srf",
-      SPO_AUTHXML,
-      content_type: "application/x-www-form-urlencoded",
-      user_agent: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)'
-    )
-    doc = ActiveSupport::XmlMini.parse(xml)
-    token = doc['Envelope']['Body']['RequestSecurityTokenResponse']['RequestedSecurityToken']['BinarySecurityToken']['__content__']
-
-    puts token
-    puts RestClient.get("https://yamaokaya1.sharepoint.com/")
-    #tokenをキーにしてクッキーをゲット
-    RestClient.post(
-      "https://yamaokaya1.sharepoint.com/_forms/default.aspx?wa=wsignin1.0",
-      token,
-      {
-        :content_type => "application/x-www-form-urlencoded",
-        :user_agent => 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)'
-      }
-    ){ |response, request, result|
-      if [301, 302, 307].include? response.code
-        str = ""
-        response.headers[:set_cookie].each do |s|
-          str << s << "; " if s =~ /^FedAuth/
-          str << s << "; " if s =~ /^rtFa/
-        end
-        puts str
-      end
-    }
-  end
-
   def show
-      @type = params[:type] if present?
+      @type = params[:type].to_sym if  params[:type].present?
       @key = 0
-      @key = params[:key].to_i if present?
+      @key = params[:key].to_i if params[:key].present?
 
-      list = nil
-      cache_key = [@type,@key].join('#')
-      begin
-        list = Redis.current.lrange(@type,(@key == 0 ? 0 : @key-1),@key+1).map{ |item|
-          OpenStruct.new(MessagePack.unpack(item)).extend(Message)
-        }
-        Rails.cache.write(cache_key,list,:expires_in => 10.minutes) unless Rails.cache.read(cache_key)
-      rescue
-        list = Rails.cache.read(cache_key)
-      ensure
-        @entry = @key == 0 ? list[0] : list[1]
-        @prev_entry = @key == 0 ? nil : list[0]
-        @next_entry = @key == 0 ? list[1] : list[2]
-      end
+      list = TopMessage.articles(:type)
+      @entry = list[key]
+      @prev_entry = @key == 0 ? nil : list[@key-1]
+      @next_entry = @key < list.size ? list[@key+1],nil
   end
 end
--- app/models/top_message.rb
+++ app/models/top_message.rb
@@ -1,6 +1,64 @@
 class TopMessage
   include ActiveModel::Model
+  attr_accessor(
+    :original_url,
+    :entry_type,
+    :title,
+    :body,
+    :created_at,
+    :updated_at,
+    :id
+  )
+  TYPES = {
+    president: {
+      name: "Microsoft.SharePoint.DataService.社長メッセージItem",
+      uri: "https://yamaokaya1.sharepoint.com/_vti_bin/listdata.svc/社長メッセージ()?$orderby=更新日時 desc&$top=50"
+    },
+    md144: {
+      name: "Microsoft.SharePoint.DataService.専務メッセージItem",
+      uri: "https://yamaokaya1.sharepoint.com/_vti_bin/listdata.svc/専務メッセージ()?$orderby=更新日時 desc&$top=50"
+    },
+    notice: {
+      name: "Microsoft.SharePoint.DataService.連絡通達Item",
+      uri: "https://yamaokaya1.sharepoint.com/_vti_bin/listdata.svc/連絡通達()?$orderby=更新日時 desc&$top=50"
+    }
+  }.freeze
 
+  def self.articles(type)
+    Rails.cache.fetch(topmessage_type: type,expires_in: 10.minutes) do
+      TopMessage.load(TYPES[type][:uri])
+    end
+  end
+
+  def self.load(uri)
+    json = RestClient.get(
+      URI::encode(uri),
+      {
+        :accept => "application/json",
+        :Cookie => TopMessage.authenticate
+      }
+    )
+    jsons = JSON.parse(json)["d"]
+    #取得したデータを登録
+    list = []
+    return jsons.map do |j|
+      type = j["__metadata"]["type"]
+      local_id = j['ID']
+      j["作成日時"] =~ /\/Date\(([0-9]+)\)\//
+      local_created_at = ($1.to_i / 1000)
+      j["更新日時"] =~ /\/Date\(([0-9]+)\)\//
+      local_updated_at = ($1.to_i / 1000)
+      TopMessage.new(
+        original_url: j["__metadata"]["uri"],
+        entry_type: j["__metadata"]["type"],
+        title: j["タイトル"],
+        body: j["本文"],
+        created_at: local_created_at,
+        updated_at: local_updated_at,
+        id: j['ID']
+      )
+    end
+  end
 
   def self.authenticate
     xml = RestClient.post(
@@ -12,8 +70,6 @@ class TopMessage
     doc = ActiveSupport::XmlMini.parse(xml)
     token = doc['Envelope']['Body']['RequestSecurityTokenResponse']['RequestedSecurityToken']['BinarySecurityToken']['__content__']
 
-    puts token
-    puts RestClient.get('https://yamaokaya1.sharepoint.com/')
     #tokenをキーにしてクッキーをゲット
     RestClient.post(
       "https://yamaokaya1.sharepoint.com/_forms/default.aspx?wa=wsignin1.0",
@@ -29,7 +85,7 @@ class TopMessage
           str << s << "; " if s =~ /^FedAuth/
           str << s << "; " if s =~ /^rtFa/
         end
-        puts str
+        return str
       end
     }
   end
