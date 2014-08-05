c51f88dacd79caf17a3d60cd63f4d045a50f8f2f
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-05 16:36:39 +0900

    wbmessage

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   #社内用トップメッセージ
   constraints host: 'wbmessagexx.yamaokaya.com' do
-    get ':type/:key(.:format)' => 'top_messages#show',as: :top_messages
+    get '/wb/:type/:key(.:format)' => 'top_messages#show',as: :top_messages
     #get '/authentication(.:format)' => 'top_messages#authentication',as: :auth_test
   end
 
